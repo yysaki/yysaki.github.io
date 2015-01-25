5347febcd0c5588a4a0548a4ead452d3d1a80901
yysaki <yysaki@yysaki.com>
2015-01-26 00:38:42 +0900

    lib/preprocess_items: Add paginate_articles

---
+++ lib/preprocess_items.rb
@@ -0,0 +1,19 @@
+def paginate_articles
+  articles_to_paginate = articles
+
+  article_groups = []
+  until articles_to_paginate.empty?
+    article_groups << articles_to_paginate.slice!(0..4)
+  end
+
+  article_groups.each_with_index do |subarticles, i|
+    first = i * 5 + 1
+    last  = (i + 1) * 5
+
+    @items << Nanoc::Item.new(
+      "<%= render 'pagination_page', :item_id => #{i} %>",
+      { :title => "Archive (articles #{first} to #{last})" },
+      "/blog/archive/#{i + 1}/"
+    )
+  end
+end
