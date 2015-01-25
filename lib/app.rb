b0edf60328562069aaf21d29f6afa9254afa469d
yysaki <yysaki@yysaki.com>
2015-01-25 19:29:42 +0900

    app.rb: Add articles

--- lib/app.rb
+++ lib/app.rb
@@ -3,3 +3,10 @@ def post_url(post, has_extension: false)
   url = "/blog/%s/%s/%s/%s/" % [yyyy, mm, dd, splittedTitle.join('-')]
   has_extension ? url + "index.html" : url
 end
+
+def articles
+  items
+  .select { |i| i.identifier.match(/posts/) }
+  .sort_by { |i| i.identifier }
+  .reverse()
+end
